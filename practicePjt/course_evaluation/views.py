c0233ffbda13aec2421958f6de7e0d083e511628
MacBook Pro <macbook@song-woo-cheol-MacBookPro.local>
2020-02-20 17:44:15 +0900

    course_evaluation 앱 추가

---
+++ practicePjt/course_evaluation/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .forms import SearchForm
+
+# Create your views here.
+
+
+def tset(request):
+    form = SearchForm()
+    return render(request, "course.html", {"form": form})
+
+
+def fetch_course(request):
+    pass
