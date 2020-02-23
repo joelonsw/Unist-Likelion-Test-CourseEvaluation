8271f3c485475b2f14985fddfa9f9ae04d01db96
MacBook Pro <macbook@song-woo-cheol-MacBookPro.local>
2020-02-23 22:44:01 +0900

    fix CRUD

--- practicePjt/course_evaluation/urls.py
+++ practicePjt/course_evaluation/urls.py
@@ -9,4 +9,19 @@ urlpatterns = [
         views.create_course_evaluation,
         name="create-course-evaluation",
     ),
+    path(
+        "fetch-course-evaluation/<int:course_id>",
+        views.fetch_course_evaluation,
+        name="fetch-course-evaluation",
+    ),
+    path(
+        "update-course-evaluation/<int:evaluation_id>",
+        views.update_course_evaluation,
+        name="update-course-evaluation",
+    ),
+    path(
+        "delete-course-evaluation/<int:evaluation_id>",
+        views.delete_course_evaluation,
+        name="delete-course-evluation",
+    ),
 ]
