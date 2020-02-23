a988c640cbe6ca270fc1df1cf74025dcf292e97f
MacBook Pro <macbook@song-woo-cheol-MacBookPro.local>
2020-02-23 19:02:29 +0900

    add http response

--- practicePjt/course_evaluation/views.py
+++ practicePjt/course_evaluation/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
-from .forms import SearchForm
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+
+from fia.models import Course
+from .serializers import CourseSerializer
 
 # Create your views here.
 
 
+@api_view(["POST"])
+@permission_classes([AllowAny])
 def tset(request):
+    course_code = request.data.get("course_code", "")
+    course_name = request.data.get("course_name", "")
+    course_professor = request.data.get("course_professor", "")
+    course_semester = request.data.get("course_semester", "")
 
-    form = SearchForm()
-    return render(request, "course.html", {"form": form})
+    course = Course.objects.all()
+    if course_name != "":
+        course = course.filter(course_name=course_name)
 
+    course = CourseSerializer(course, many=True).data
 
-def fetch_course(request):
-    pass
+    # return render(request, "course.html", {"data": data})
+    return Response(course, status=200)
+
