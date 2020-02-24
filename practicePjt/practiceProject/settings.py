b7adf99425a851b80168ba04ae3d4cfff6d0c964
joel <joel610@likelion.org>
2020-02-24 16:37:30 +0900

    deployPractice

--- practicePjt/practiceProject/settings.py
+++ practicePjt/practiceProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "#bsakon!(jftyacx+j0cbh^a(u&q^5#+owrr!3kpejx+0^515y"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ ]
 
 
 # Application definition
